d50f3f901b96eef57ee35046c449e9af36d4c1ba
AyushLochan <139963233+AyushLochan@users.noreply.github.com>
2024-03-26 09:59:11 +0530

    Add files via upload

---
+++ sol4/main.js
@@ -0,0 +1,9 @@
+const math = require('./math');
+
+const a=20;
+const b=10;
+
+console.log("Addition:", math.add(a, b));
+console.log("Subtraction:", math.subtract(a, b));
+console.log("Multiplication:", math.multiply(a, b));
+console.log("Division:", math.divide(a, b));
